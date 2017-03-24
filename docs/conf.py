9c7a77b63623ddf9d5aea24ed98b91861d797e79
William Gibb <williamgibb@gmail.com>
2017-03-24 00:43:10 -0400

    fixes

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ if os.getenv('SPELLCHECK'):
 
 source_suffix = '.rst'
 master_doc = 'index'
-project = 'pyfuckery'
+project = 'fuckery'
 year = '2017'
 author = 'William Gibb'
 copyright = '{0}, {1}'.format(year, author)
