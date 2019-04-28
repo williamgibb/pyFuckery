90afd6bce003fa960740ad73847a027fe3e5c3f4
William Gibb <williamgibb+github@gmail.com>
2019-04-28 12:36:08 -0400

    Fix up comment part of the grammer

--- src/fuckery/constants.py
+++ src/fuckery/constants.py
@@ -40,6 +40,10 @@ MEMORY_MAX_VALUE = 255
 IGNORE_REGEX = f'\\{SYM_PTR_DEC}\\{SYM_PTR_INC}\\{SYM_DATA_DEC}\\{SYM_DATA_INC}' \
     f'\\{SYM_IO_INPUT}\\{SYM_IO_OUTPUT}\\{SYM_JMP_FWD}\\{SYM_JMP_BACKWARD}'
 
+# Credit where credit is due - largely influenced by from
+# https://webcache.googleusercontent.com/search?q=cache:cdkXLzIKMA0J:https://groups.google.com/a/cdglabs.org/d/topic
+# /ohm/Jvwx1jvPOqY+&cd=8&hl=en&ct=clnk&gl=us
+# which was inspired by https://www.researchgate.net/publication/255592935_Implementing_Brainfuck_in_COLA_Version_2
 GRAMMER = f'''
 
 {SYM_PROGRAM}: {SYM_EXPRESSIONS}*
@@ -58,43 +62,6 @@ IO_INPUT: "{SYM_IO_INPUT}"
 IO_OUTPUT: "{SYM_IO_OUTPUT}"
 JMP_FWD: "{SYM_JMP_FWD}"
 JMP_BCK: "{SYM_JMP_BACKWARD}"
-
+COMMENT: /[^{IGNORE_REGEX}]+/
+%ignore COMMENT
 '''
-
-# Credit where credit is due - largely influenced by from
-# https://webcache.googleusercontent.com/search?q=cache:cdkXLzIKMA0J:https://groups.google.com/a/cdglabs.org/d/topic
-# /ohm/Jvwx1jvPOqY+&cd=8&hl=en&ct=clnk&gl=us
-# which was inspired by https://www.researchgate.net/publication/255592935_Implementing_Brainfuck_in_COLA_Version_2
-OLD_GRAMMER = r'''
-
-{program}: {expression}*
-
-{loop}: JMP_FWD {expression}* JMP_BCK
-
-{actions}: PTR_DEC | PTR_INC | DATA_DEC | DATA_INC | IO_INPUT | IO_OUTPUT
-
-{expression}: {actions} | {loop}
-
-PTR_DEC: "{ptr_dec}"
-PTR_INC: "{ptr_inc}"
-DATA_DEC: "{data_dec}"
-DATA_INC: "{data_inc}"
-IO_INPUT: "{io_input}"
-IO_OUTPUT: "{io_output}"
-JMP_FWD: "{jmp_forward}"
-JMP_BCK: "{jmp_backwards}"
-COMMENT.ignore: /[^{ignore_chars}]+/
-'''.format(ptr_inc=SYM_PTR_INC,
-           ptr_dec=SYM_PTR_DEC,
-           data_dec=SYM_DATA_DEC,
-           data_inc=SYM_DATA_INC,
-           io_input=SYM_IO_INPUT,
-           io_output=SYM_IO_OUTPUT,
-           jmp_forward=SYM_JMP_FWD,
-           jmp_backwards=SYM_JMP_BACKWARD,
-           ignore_chars=IGNORE_REGEX,
-           program=SYM_PROGRAM,
-           expression=SYM_EXPRESSIONS,
-           loop=SYM_LOOP,
-           actions=SYM_ACTIONS
-           )
