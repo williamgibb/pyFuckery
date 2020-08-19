7cbd6515b9e816e4ea42d155f9d97d8b8a2fd4d0
William Gibb <williamgibb+github@gmail.com>
2020-08-19 16:10:07 +0000

    Bump version: 0.5.1 → 0.5.2

--- src/fuckery/__init__.py
+++ src/fuckery/__init__.py
@@ -4,5 +4,5 @@ from fuckery import memory
 from fuckery import parser
 from fuckery import vm
 
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 __all__ = ['constants', 'exc', 'memory', 'parser', 'vm']
